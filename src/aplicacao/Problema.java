e1f7a424efd418f91645dc31fa0c11b95420ef70
Andre Vilela <andrevibegoogle@hotmail.com>
2020-05-20 20:38:27 -0300

    Programa finalizado

--- src/aplicacao/Problema.java
+++ src/aplicacao/Problema.java
@@ -14,18 +14,53 @@ public class Problema {
 		double largura, altura, area, perimetro;
 		int opcao = 0;
 		
-		System.out.println("Digite a largura do retangulo:");
+		System.out.println("Digite a largura do retangulo: ");
 		largura = sc.nextDouble();
 		while (largura <= 0) {
-			System.out.println("O valor deve ser positivo. Tente novamente:");
+			System.out.println("O valor deve ser positivo. Tente novamente: ");
 			largura = sc.nextDouble();
 		}
 		
-		System.out.println("Digite a altura do retangulo:");
+		System.out.println("Digite a altura do retangulo: ");
 		altura = sc.nextDouble();
 		while (altura <= 0) {
-			System.out.println("O valor deve ser positivo. Tente novamente:");
+			System.out.println("O valor deve ser positivo. Tente novamente: ");
 			altura = sc.nextDouble();
+		}
+		
+		while (opcao < 3) {
+			System.out.println();
+			System.out.println("MENU:");
+			System.out.println("1 - Mostrar area");
+			System.out.println("2 - Mostrar perimetro");
+			System.out.println("3 - Sair");
+			System.out.println("Digite uma opcao: ");
+			opcao = sc.nextInt();
+			while (opcao > 3 || opcao < 1) {
+				System.out.println();
+				System.out.println("OPÇÃO INVALIDA");
+				
+				System.out.println();
+				System.out.println("MENU:");
+				System.out.println("1 - Mostrar area");
+				System.out.println("2 - Mostrar perimetro");
+				System.out.println("3 - Sair");
+				System.out.println("Digite uma opcao: ");
+				opcao = sc.nextInt();
+			}
+			
+			System.out.println();
+			
+			if (opcao == 1) {
+				area = largura * altura;
+				System.out.println("AREA = " + area);
+			} else if (opcao == 2) {
+				perimetro = 2 *(largura + altura);
+				System.out.println("PERIMETRO = " + perimetro);
+			} else {
+				System.out.println("FIM DO PROGRAMA!");
+			}
+			
 		}
 		
 		sc.close();
